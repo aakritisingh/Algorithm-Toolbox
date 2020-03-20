c4eae0a6b6006da9c9c3da805a280fcfacbd9832
aakritisingh <aakritisingh13@gmail.com>
2020-03-20 09:57:48 -0700

    Add files via upload

---
+++ week 5/change_dp.py
@@ -0,0 +1,27 @@
+# Uses python3
+import sys
+import math
+
+def get_change(m):
+    #write your code here
+    coins = [1,3,4]
+    min_number_of_coins = [None] * (m + 1)
+
+    min_number_of_coins[0] = 0
+
+    for i in range(1, m + 1):
+        min_number_of_coins[i] = math.inf
+        for whole_coin in coins:
+            if i >= whole_coin:
+                tmp = min_number_of_coins[i - whole_coin] + 1
+                if tmp < min_number_of_coins[i]:
+                    min_number_of_coins[i] = tmp
+            else:
+                break
+
+    return min_number_of_coins[m]
+    
+
+if __name__ == '__main__':
+    m = int(sys.stdin.read())
+    print(get_change(m))
